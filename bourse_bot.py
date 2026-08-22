5d24a4f2e77c9f520e8e1e87cede7708e2d23f13
afshin1apple-dev <afshin1apple@gmail.com>
2026-08-22 13:55:41 +0330

    Update bourse_bot.py

--- bourse_bot.py
+++ bourse_bot.py
@@ -1,31 +1,27 @@
 import requests
 
-URL = "https://www.ime.co.ir/offer-stat.html"
+URL = "https://tts.ime.co.ir/"
 
 def main():
-
-    print("در حال اتصال به بورس کالا...")
+    print("در حال اتصال به تابلو بورس کالا...")
 
     try:
         response = requests.get(
             URL,
-            timeout=30,
             headers={
                 "User-Agent": "Mozilla/5.0"
-            }
+            },
+            timeout=30
         )
 
-        print("Status:", response.status_code)
-        print("Size:", len(response.text))
+        print("STATUS:", response.status_code)
+        print("SIZE:", len(response.content))
+        print("CONTENT-TYPE:", response.headers.get("content-type"))
 
-        print("\n--- شروع اطلاعات دریافت شده ---\n")
-
-        print(response.text[:3000])
-
-        print("\n--- پایان تست ---")
+        print("\n--- RESPONSE ---")
+        print(response.text[:5000])
 
     except Exception as e:
-
         print("ERROR:", e)
 
 
